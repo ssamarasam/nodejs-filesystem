4202752232b732ff008fe5b1b94e398e5a4a2371
ssamarasam <samcsegct@gmail.com>
2023-07-13 23:52:52 +0530

    added read file api

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require("express");
 const app = express();
-const fs = require("node:fs/promises");
+const fs = require("node:fs");
 
-app.get("/", (request, response) => {
-  console.log("got get request inside app js");
-  return response.status(200).json({
-    message: "API working",
-  });
-});
+// app.get("/", (request, response) => {
+//   console.log("got get request inside app js");
+//   return response.status(200).json({
+//     message: "API working",
+//   });
+// });
 
-app.get("/createFile", async (request, response) => {
+app.get("/createFile", (request, response) => {
   const fileDate = new Date().toUTCString().slice(5, 16).split(" ").join("-");
   const fileHours = new Date().getHours().toString();
   const fileMinutes = new Date().getMinutes().toString();
@@ -18,16 +18,68 @@ app.get("/createFile", async (request, response) => {
   const fileName = `D${fileDate}_T${fileHours}${fileMinutes}${fileSeconds}`;
   let fileData = new Date().toString();
   console.log(fileName);
-  try {
-    await fs.appendFile(`files/${fileName}.txt`, fileData);
-    return response.status(200).json({
-      message: "file created successfully",
-    });
-  } catch (err) {
-    return response.status(400).json({
-      message: err,
-    });
-  }
+
+  fs.appendFile(`files/${fileName}.txt`, fileData, (err) => {
+    if (err) {
+      return response.status(400).json({
+        message: err,
+      });
+    } else {
+      return response.status(200).json({
+        message: "file created successfully",
+        fileName: fileName,
+      });
+    }
+  });
+});
+
+app.get("/readFile/:name", (request, response) => {
+  const readFileName = request.params.name;
+  console.log(readFileName);
+  fs.readFile(`files/${readFileName}.txt`, "utf-8", (err, data) => {
+    if (err) {
+      return response.status(400).json({
+        message: err,
+        note: "do not add .txt at the end; it is added in the code",
+      });
+    } else {
+      return response.status(200).send(data);
+    }
+  });
 });
 
 module.exports = app;
+
+// const express = require("express");
+// const app = express();
+// const fs = require("node:fs/promises");
+
+// app.get("/", (request, response) => {
+//   console.log("got get request inside app js");
+//   return response.status(200).json({
+//     message: "API working",
+//   });
+// });
+
+// app.get("/createFile", async (request, response) => {
+//   const fileDate = new Date().toUTCString().slice(5, 16).split(" ").join("-");
+//   const fileHours = new Date().getHours().toString();
+//   const fileMinutes = new Date().getMinutes().toString();
+//   const fileSeconds = new Date().getSeconds().toString();
+
+//   const fileName = `D${fileDate}_T${fileHours}${fileMinutes}${fileSeconds}`;
+//   let fileData = new Date().toString();
+//   console.log(fileName);
+//   try {
+//     await fs.appendFile(`files/${fileName}.txt`, fileData);
+//     return response.status(200).json({
+//       message: "file created successfully",
+//     });
+//   } catch (err) {
+//     return response.status(400).json({
+//       message: err,
+//     });
+//   }
+// });
+
+// module.exports = app;
--- index.js
+++ index.js
@@ -4,6 +4,19 @@ const httpServer = express();
 const app = require("./app");
 
 // httpServer.use("/api", )
+httpServer.get("/", (request, response) => {
+  console.log("got get request inside app js");
+  return response.status(200).end(`Welcome to nodejs file system
+    To Create file: /createFile,
+    to read file: /readFile/:fileName,
+  `);
+
+  // return response.status(200).json({
+  //   message: "Welcome to nodejs file system",
+  //   ToCreateFile: "/createFile",
+  //   toReadFile: "/readFile/:filename",
+  // });
+});
 
 httpServer.use("/api", app);
 
